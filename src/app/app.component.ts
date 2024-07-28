0a4508bdb44a4006b9ab39535750828178596e06
VaibhavK2615 <vaibhavkalvankar97@gmail.com>
2024-07-28 11:59:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PlacementsComponent } from './placements/placements.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent, PlacementsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/placements/placements.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placements',
+  standalone: true,
+  imports: [],
+  templateUrl: './placements.component.html',
+  styleUrl: './placements.component.css'
+})
+export class PlacementsComponent {
+  
+}
